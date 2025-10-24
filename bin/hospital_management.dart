0180eb0820ede2fddf8216aa42e9a2da22be16b7
sothearasaroeun <sotheara.saroeun.911@gmail.com>
2025-10-24 14:34:51 +0700

    first commit

---
+++ bin/hospital_management.dart
@@ -0,0 +1,5 @@
+import 'package:hospital_management/main.dart' as hospital_management;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hospital_management.calculate()}!');
+}
---
+++ test/hospital_management_test.dart
@@ -0,0 +1,8 @@
+import 'package:hospital_management/main.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
