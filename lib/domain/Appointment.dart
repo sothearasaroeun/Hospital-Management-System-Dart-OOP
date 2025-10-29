554bd4d91385c039744b8f044ae811c77ddd7390
sothearasaroeun <sotheara.saroeun.911@gmail.com>
2025-10-29 12:04:12 +0700

    class update

--- lib/domain/Appointment.dart
+++ lib/domain/Appointment.dart
@@ -1,17 +1,21 @@
-import 'Patient.dart';
-import 'Doctor.dart';
+import 'patient.dart';
+import 'doctor.dart';
+enum AppointmentStatus {schecduled, rescheduled, canceled, completed, noShow}
 
 class Appointment{
   final String _id;
   final DateTime _dateTime;
   final Patient _patient;
   final Doctor _doctor;
+  final AppointmentStatus _appointmentStatus;
 
-  Appointment(this._id, this._dateTime, this._patient, this._doctor);
+  Appointment(this._id, this._dateTime, this._patient, this._doctor, this._appointmentStatus);
 
   String get id => _id;
   DateTime get dateTime => _dateTime;
   Patient get patient => _patient;
   Doctor get doctor => _doctor;
+  AppointmentStatus get appointmentStatus => _appointmentStatus;
 
 }
+
--- test/hospital_management_test.dart
+++ test/hospital_management_test.dart
@@ -2,7 +2,5 @@ import 'package:hospital_management/main.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+
 }
